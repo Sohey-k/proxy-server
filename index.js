97cb4cb40221fe90fe632f866231ca5fb21ee341
team-K <reepoo0528@gmail.com>
2024-08-18 18:11:04 +0900

    Add CORS-v2 code

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
+    if (req.method === "OPTIONS") {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 
