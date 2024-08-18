7187721845ff68d8a4a7e56946a6c475323b06d4
team-K <reepoo0528@gmail.com>
2024-08-18 18:04:18 +0900

    Add CORS code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
-const app = express()　
-const { createProxyMiddleware } = require("http-proxy-middleware") 
+const app = express()
+const { createProxyMiddleware } = require("http-proxy-middleware")
 const rateLimit = require("express-rate-limit")
 require("dotenv").config()
 const url = require("url")
@@ -8,17 +8,26 @@ const url = require("url")
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 10000, 
+    max: 10000,
 })
+
+// CORSを許可するミドルウェアを追加
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 
 app.use(limiter)
 
-app.get("/", (req, res) => {  
+app.get("/", (req, res) => {
     res.send("This is my proxy server")
-})　
+})
 
 app.use("/weather-data", (req, res, next) => {
-    const city = url.parse(req.url).query 
+    const city = url.parse(req.url).query
     createProxyMiddleware({
         target: `${process.env.BASE_API_URL_WEATHERAPI}${city}&aqi=no`,
         changeOrigin: true,
@@ -29,7 +38,7 @@ app.use("/weather-data", (req, res, next) => {
 })
 
 app.use("/corona-tracker-country-data", (req, res, next) => {
-    const city = url.parse(req.url).query 
+    const city = url.parse(req.url).query
     createProxyMiddleware({
         target: `${process.env.BASE_API_URL_CORONA_COUNTRY}/${city}`,
         changeOrigin: true,
